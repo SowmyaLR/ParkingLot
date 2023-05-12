8f2ea5b45cefbc3371f2a79e1446ef8c0de6d89d
SowmyaLR <sowmya@geektrust.in>
2023-05-12 09:36:13 +0530

    test cases added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from service.parkinglot_service import ParkingLotService
 import sys
 
+
 def main():
     input_file = sys.argv[1]
     parking_srv = ParkingLotService()
     parking_srv.calculate_bill(input_file)
+
 
 if __name__ == "__main__":
     main()
